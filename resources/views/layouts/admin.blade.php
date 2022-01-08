68e540e3ca658e86528eb28bbf9e8b5f98ae831b
kawaguchi saki <amsn17.saki15@gmail.com>
2022-01-08 05:58:41 +0000

    ナビゲーションバー勤務記録追加

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -24,7 +24,16 @@
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul class="navbar-nav mr-auto">
                             <li class="nav-item active">
-                                <a class="nav-link" href="{{ route('admin_home') }}">Home <span class="sr-only">(current)</span></a>
+                                <a class="nav-link" href="{{ route('admin_home') }}">ホーム <span class="sr-only">(current)</span></a>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink-site" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                勤務記録
+                                </a>
+                                <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink-site">
+                                    <a class="dropdown-item" href="{{ route('new') }}">登録</a>
+                                    <a class="dropdown-item" href="{{ route('admin_attendancerecords') }}">一覧</a>
+                                </div>
                             </li>
                             <li class="nav-item dropdown">
                                 <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink-site" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
