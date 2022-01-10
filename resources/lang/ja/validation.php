f483557fafe3b461a15240aa37344bddc461b3cd
kawaguchi saki <amsn17.saki15@gmail.com>
2022-01-10 01:50:47 +0000

    バリデーションメッセージ訂正

--- resources/lang/ja/validation.php
+++ resources/lang/ja/validation.php
@@ -112,7 +112,7 @@ return [
     'starts_with' => 'The :attribute must start with one of the following: :values',
     'string' => 'The :attribute must be a string.',
     'timezone' => 'The :attribute must be a valid zone.',
-    'unique' => ':attribute はすでに存在しています。.',
+    'unique' => ':attribute はすでに存在しています。',
     'uploaded' => 'The :attribute failed to upload.',
     'url' => 'The :attribute format is invalid.',
     'uuid' => 'The :attribute must be a valid UUID.',
