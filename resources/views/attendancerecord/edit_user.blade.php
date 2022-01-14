3e458da8a1ec48edbd6838800bfc6f64a7cc8c2b
kawaguchi saki <amsn17.saki15@gmail.com>
2022-01-14 06:22:25 +0000

    アイコン変更機能削除

--- resources/views/attendancerecord/edit_user.blade.php
+++ resources/views/attendancerecord/edit_user.blade.php
@@ -37,22 +37,6 @@
                             <input type="text" size=30 name='passward' placeholder="新しいパスワード" autocomplete="off">
                         </div>
                     </div>
-                    <div class="form-group row">
-                        <label class="col-4">アイコン</label>
-                        <div class="col-8">
-                            <input type="file" class="form-control-file" name="icon_path">
-                            <div class="form-text text-info">
-                                設定中: {{ $user->icon_path }}
-                            </div>
-                            <div class="form-check">
-                                <label class="form-check-label">
-                                    <input type="checkbox" class="form-check-input" name="remove" value="true">画像を削除
-                                </label>
-                            </div>
-                        </div>
-                        
-                        
-                    </div>
                     {{ csrf_field() }}
                     <input type="submit" class="btn btn-primary btn-lg btn-block" value="編集">
                 </form>
