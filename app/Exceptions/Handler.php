690202329ec9823c0b63d153139ca313edb7cd58
kawaguchi saki <amsn17.saki15@gmail.com>
2022-01-17 08:22:57 +0000

    ログアウト419エラー処理

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -46,6 +46,9 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
-        return parent::render($request, $exception);
+        //instanceofでTokenMismatchExceptionのインスタンスか判定
+        if ($exception instanceof \Illuminate\Session\TokenMismatchException) {
+            return redirect('/');
+        }
     }
 }
