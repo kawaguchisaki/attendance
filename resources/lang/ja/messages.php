a709fe38d52e8b16399beab3d909cc40056c93f1
kawaguchi saki <amsn17.saki15@gmail.com>
2022-01-31 05:54:33 +0000

    メッセージ日本語表記設定追加

--- resources/lang/ja/messages.php
+++ resources/lang/ja/messages.php
@@ -2,8 +2,11 @@
 
 return [
     'Login' => 'ログイン',
+    'Name' => '名前',
     'E-Mail Address' =>'メールアドレス',
     'Password' => 'パスワード',
+    'Confirm Password' => 'パスワード確認',
     'Remember Me' => 'ログイン情報を記録する',
     'Logout' => 'ログアウト',
+    'Register' => '新規登録',
     ];
