35f6add5ae3f39112b27c98af2de70f4337649d6
kawaguchi saki <amsn17.saki15@gmail.com>
2022-02-04 05:28:31 +0000

    navbarホームリンク日本語表記に変更、余白削除

--- resources/views/layouts/user.blade.php
+++ resources/views/layouts/user.blade.php
@@ -19,7 +19,7 @@
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul class="navbar-nav mr-auto">
                             <li class="nav-item active">
-                                <a class="nav-link" href="{{ route('user_home') }}">Home <span class="sr-only">(current)</span></a>
+                                <a class="nav-link" href="{{ route('user_home') }}">ホーム<span class="sr-only">(current)</span></a>
                             </li>
                             <li class="nav-item">
                                 <a class="nav-link" href="{{ route('user_sites') }}">
